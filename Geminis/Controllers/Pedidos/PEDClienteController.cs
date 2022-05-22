95826245dda7416eca4d1d3a6c54664ecac23e2c
DESKTOP-J3MI77V\DELL-G3 <alejandrolopez445@gmail.com>
2022-05-22 08:29:42 -0600

    Se corrige la cadena

---
+++ Geminis/Controllers/Pedidos/PEDClienteController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Geminis.Controllers.Pedidos
+{
+    public class PEDClienteController : Controller
+    {
+        // GET: PEDCliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
